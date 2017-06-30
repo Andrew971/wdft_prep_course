01e378e9c2391bdacbe815595cd5b091b194b984
Andrew SORDIER <andrewsordier@MacBook-de-Andrew.local>
2017-07-01 07:53:17 +0900

    assignement 2 DONE

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,8 +2,11 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-
-
+var i = 0;
+for (var i = 0; i < 101;i) {
+    console.log(i);
+    i = i + 1;
+}
 
 /*
 Exercise #2
@@ -17,3 +20,14 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+//première methode
+var a = "";
+for (counter=0; counter < 8; counter++) {
+    console.log(a);
+    a = a + "#";
+}
+//deuxième methode
+var b = "#";
+for(i = 1; i < 8; i++){
+    console.log(b.repeat(i));
+}
