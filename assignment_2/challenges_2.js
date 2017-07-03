06f070c425646d6c36448a8345a2068c66c61a10
Andrew SORDIER <andrewsordier@MacBook-de-Andrew.local>
2017-07-04 01:47:37 +0900

    chalolenges, exercices and assignements for Javascript chapter is DONE !

--- assignment_2/challenges_2.js
+++ assignment_2/challenges_2.js
@@ -1,17 +1,44 @@
-//magic eightball
+//magic eightball with if else statement 
 
 var numberofreplys = 2;
 function ask(){
     var choice = Math.floor(Math.random() * numberofreplys +1);
     var reply = 'You need to make some options using if/else'
+if (choice == 1) {
+    console.log("You are ugly");
+}
+else if (choice == 2){ 
+    console.log("You are beautiful");
+}
+else {
+console.log(reply);
+}
+    console.log("Your ball was the number", choice);
 
-    /*
-    write conditional logic here using if, else if, and else
-
-
-    */
-
-    console.log(choice);
 }
 
 ask();
+
+//with switch statement
+
+var numberofreplys1 = 2;
+function ask1(){
+    var choice1 = Math.floor(Math.random() * numberofreplys1 +1);
+    var reply1 = 'You need to make some options using if/else'
+switch(choice1) {
+    case 1:
+       console.log("You are ugly");
+        break;
+    case 2:
+    console.log("You are beautiful");
+        break;
+    default:
+console.log(reply1);
+}
+    
+console.log("Your ball was the number", choice1);
+
+}
+
+ask1();
+
